ecfa04148d5a76f18ffaebce8b8e3706f7dc8501
INVOIDER <5261323@gmail.com>
2023-04-19 18:06:38 +0300

    Создал сервер NodeJS
    Был создан первоначальный сервер NodeJS с использованием модулей express, nodemon и pg.
    Также для тестирования было собрано docker приложение, состоящее из 3х контейнеров: postgres, pgadmin и nodeJS

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express'); //Подключаем модуль express
+
+const server = express(); // Создание экземпляра express
+const PORT = process.env.PORT || 3000;
+
+server.use(express.static('./client')); //Имя папки где хранится система
+
+server.listen(PORT,() =>{
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
+
+server.get('/', function (req, res){
+    console.log('load /')
+    res.render('index.html');
+});

2db87a19d9a67004873c590be073efa043d7404a
INVOIDER <5261323@gmail.com>
2023-04-30 16:30:07 +0300

    Добавил новые переменные среды
    Добавил переменные среды

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg').Pool //Реализация подключения к бд с помощью pool
 const pool = new Pool({
-    user: "postgres",
-    password: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'filmoteka'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool
